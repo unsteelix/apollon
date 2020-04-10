7add8a34e332516c30621cf279d60c03b93009cf
unsteelix <unsteelix@yandex.ru>
2020-04-10 15:06:26 +0400

    remake webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
 
 module.exports = {
     devtool: "source-map", // Enable sourcemaps for debugging webpack's output.
     entry: "./src/index.jsx", // входная точка - исходный файл
     output:{
         path: path.resolve(__dirname, './dist'),     // путь к каталогу выходных файлов - папка dist
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: "bundle.js"       // название создаваемого файла
     },
     module:{
@@ -20,24 +21,30 @@ module.exports = {
                 }
             },
             {
-                test: /\.(s*)css$/,
-                include: path.resolve(__dirname, './src/style'),
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                          publicPath: './dist',
-                        }
-                    },
-                    'css-loader',
-                    'sass-loader'
-                ]
+                test: /\.(sass|scss)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
             }
         ]
     },
     plugins: [
+        /*
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/public/index.html",
+            inject: 'body'
+        }),
+        */
         new MiniCssExtractPlugin({
            filename: 'style.css',
         }),
-    ]
+    ],
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './',  //source of static assets
+        port: 7700, // port to run dev-server
+    }  
 }
